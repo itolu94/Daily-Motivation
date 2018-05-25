88d1fea5f74637515eb21f96883f75d674052b64
Tolu Idowu <itolu94@gmail.com>
2018-05-25 17:52:21 -0400

    initial commit

---
+++ app/Components/Layout.jsx
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+
+class Layout extends Component {
+    constructor(){
+        super();
+    }
+    render(){
+        return(
+            <div>
+                <h1>Hello World</h1>
+            </div>
+        )
+    }
+}
+export default Layout;
---
+++ app/app.jsx
@@ -0,0 +1,6 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import Layout from './Components/Layout.jsx';
+const app = document.querySelector('#app');
+
+ReactDOM.render(<Layout />, app);
